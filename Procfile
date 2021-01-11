8019ce54a0e34d1677aaf513e673a76cdfb6e3b6
jeeve <jyvaisj@gmail.com>
2021-01-11 11:13:34 +0100

    df

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn indexapp.wsgi:application --timeout 6000
+web: gunicorn indexapp:application --timeout 6000
--- indexapp/__init__.py
+++ indexapp/__init__.py
@@ -4,4 +4,15 @@ from flask import Flask
 from .views import app
 from . import models
 
+from flask import Flask
+from gpsapp import app as gps_app
+from indexapp import app as index_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
+app = Flask(__name__)
+
+application = DispatcherMiddleware(index_app , {
+    '/gps': gps_app
+})
 
